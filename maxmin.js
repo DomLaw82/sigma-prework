7dc655b197f7f42e4ab5bc9616239fca38ca52a7
Dominic Lawson <dominic.l12@hotmail.co.uk>
2022-10-07 00:45:17 +0100

    removing tests from .js file

--- maxmin.js
+++ maxmin.js
@@ -3,8 +3,3 @@ function maxmin(arr) {
   arr = arr.sort(numericalSort);
   return [arr[0], arr.pop()];
 }
-console.log(
-  maxmin([
-    2, 3234, 421, 4545, 435, 12, 53, 8, 686, 46, 85, 343, 754, 23243, 53, -11,
-  ])
-);
